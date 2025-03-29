89055783377898b9d5e417d8fc34be21d4784cfc
entaku <entaku19890818@gmail.com>
2025-03-29 10:40:57 +0900

    first commit

---
+++ TCA-sample/TCA_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_sampleApp.swift
+//  TCA-sample
+//
+//  Created by 遠藤拓弥 on 2025/03/29.
+//
+
+import SwiftUI
+
+@main
+struct TCA_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA-sampleTests/TCA_sampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_sampleTests.swift
+//  TCA-sampleTests
+//
+//  Created by 遠藤拓弥 on 2025/03/29.
+//
+
+import Testing
+@testable import TCA_sample
+
+struct TCA_sampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
